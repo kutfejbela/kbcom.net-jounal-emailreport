431d53247ad558c0bde3101d10f420f899c53d4e
root <root@szerver05.petav.local>
2018-08-28 14:21:06 +0200

    Journal

--- install.bash
+++ install.bash
@@ -1,12 +1,12 @@
 # Ask: email from,to,subject,envelopefrom
 # Ask: CA,krb5-ticket
 
-/bin/ln -s /opt/kbcom.net-syslog-emailreporter/local/etc/rsyslog.d/90-kbcom.net-syslog-emailreporter.conf /etc/rsyslog.d/90-kbcom.net-syslog-emailreporter.conf
-/bin/ln -s /opt/kbcom.net-syslog-emailreporter/local/etc/systemd/* /lib/systemd/system
-/bin/systemctl enable --now syslog-emailreporter.timer
+/bin/ln -s /opt/kbcom.net-jounal-emailreport/local/etc/rsyslog.d/90-kbcom.net-jounal-emailreport.conf /etc/rsyslog.d/90-kbcom.net-jounal-emailreport.conf
+/bin/ln -s /opt/kbcom.net-jounal-emailreport/local/etc/systemd/* /lib/systemd/system
+/bin/systemctl enable --now jounal-emailreport.timer
 /bin/systemctl enable --now syslog-krb5.service
 /bin/systemctl enable --now syslog-krb5.timer
 
-/bin/mkdir /opt/kbcom.net-syslog-emailreporter/var
-/bin/chown -R syslog:adm /opt/kbcom.net-syslog-emailreporter/var
+/bin/mkdir /opt/kbcom.net-jounal-emailreport/var
+/bin/chown -R syslog:adm /opt/kbcom.net-jounal-emailreport/var
 /bin/systemctl restart rsyslog.service
